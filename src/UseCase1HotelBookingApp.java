9420120f3fc15c9fc112c8e8b8ef153d16ccdf6e
DishaKhatki23 <dishakhatki23@gmail.com>
2026-03-30 15:01:28 +0530

    UC1

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,32 @@
+
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Application entry point.
+     *
+     * This method is the first method executed
+     * when the program is launched by the JVM.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Application Name & Version
+        String appName = "Book My Stay - Hotel Booking Management System";
+        String version = "Version 1.0";
+
+        // Welcome Message
+        System.out.println("==============================================");
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("==============================================");
+
+        // Display Application Details
+        System.out.println(appName);
+        System.out.println(version);
+
+        // System Initialization Message
+        System.out.println("\nSystem initialized successfully.");
+
+        // End of Program
+    }
+}
